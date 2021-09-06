131f15418d584c208a02283fe4d1bad73f06a753
KevoHoff <67078785+KevoHoff@users.noreply.github.com>
2021-09-06 00:27:17 -0400

    Documentation Upgrade

--- MetadataExtraction/RawEntDetector.py
+++ MetadataExtraction/RawEntDetector.py
@@ -1,13 +1,32 @@
 import spacy
 from dateutil import parser
 
+"""
+Handles analysis of transcript key-value pairs
+"""
 class RawEntDetector():
     nlp = spacy.load('en_core_web_trf')  # NLP Class Variable
 
     def __init__(self, META):
         self.META = META
         self.form = {}
-
+    
+    """
+    Extracts the first name, last name, and graduation year if the information is found in a transcript
+    
+    Parameters
+    ----------
+    NA: (list) a collection of strings, where strings are the names of any piece of missing metadata
+               (e.g., 'GRAD')
+   
+    text: (list) a collection of strings representing text lines found in a document
+    
+    Returns
+    -------
+    form: (dictionary) a mapping of metadata names to extracted information. In other words, the
+                       desired metadata are the keys and the selected information from fields in
+                       a transcript are the values (e.g. what the first name is)
+    """
     def detectEntity(self, NA, text):
         if 'First' in NA:
             first, last = self.__getName__(text)
@@ -18,7 +37,26 @@ class RawEntDetector():
         #     grad = self.__getGrad__(text)
 
         return self.form
-
+    
+    """
+    Finds the first and last name in a list of possible names from the transcript. Tries
+    to take in various formats (e.g., First Last; Last, First; First Mi Last; Last, First Mi)
+    
+    Parameters
+    ----------   
+    text: (list) a collection of strings representing text lines found in a document
+    
+    Returns
+    -------
+    first: (string) the text representation of a person's first name in the document
+    
+    last: (string) the text representation of a person's first name in the document
+    
+    Priority Work
+    -------------
+    This algorithm needs improvements as it gets confused when there is a middle name.
+    Likely can be optimized with more concise code
+    """
     def __getName__(self, text):
         names = []
 
@@ -27,8 +65,6 @@ class RawEntDetector():
             for ent in doc.ents:
                 if ent.label_ == 'PERSON':
                     names.append(line)
-
-        print(names)
 
         if len(names) == 1:
             name = names[0]
